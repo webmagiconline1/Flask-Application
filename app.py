babd8d132f2c5479c3740597440b1b1bee5ca263
Dabeer Shaikh <dab8106@gmail.com>
2021-01-31 15:08:32 +0530

    Reverted code

---
+++ app.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+=======
+from flask import Flask
+from flask_redis import FlaskRedis
+
+app = Flask(__name__)
+app.config['REDIS_URL'] = 'redis://redis:6379/0'
+
+redis = FlaskRedis(app)
+
+
+@app.route('/')
+def counter():
+    return str(redis.incr('web2_counter'))
+>>>>>>> parent of 9475e72 (Updated Redis Connection or key string)
